ca8ad113e644ff2528d268f20b6ca871ca7e935b
Guilherme <guilherme.dds.dev@gmail.com>
2025-10-07 22:31:32 -0300

    Atualizar barra e renew

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let passwordLength = 16;
 const inputEl = document.querySelector("#password");
-
+const upperCaseCheckEl = document.querySelector("#uppercase-check");
+const numberCheckEl = document.querySelector("#number-check");
+const symbolCheckEl = document.querySelector("#symbol-check");
+const securityIndicatorBarEl = document.querySelector(
+  "#security-indicator-bar"
+);
 function generatePassword() {
-  const chars =
-    "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
+  let chars = "abcdefghjklmnpqrstuvwxyz";
+
+  const upperCaseChars = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+
+  const numbersChars = "123456789";
+
+  const symbolChars = "?!@&*()[]";
+
+  if (upperCaseCheckEl.checked) {
+    chars += upperCaseChars;
+  }
+
+  if (numberCheckEl.checked) {
+    chars += numbersChars;
+  }
+
+  if (symbolCheckEl.checked) {
+    chars += symbolChars;
+  }
 
   let password = "";
 
@@ -13,6 +35,59 @@ function generatePassword() {
   }
 
   inputEl.value = password;
+  calculateQuality();
+  calculateFontSize();
+}
+
+function calculateQuality() {
+  const percent = Math.round(
+    (passwordLength / 64) * 25 +
+      (upperCaseCheckEl.checked ? 15 : 0) +
+      (numberCheckEl.checked ? 25 : 0) +
+      (symbolCheckEl.checked ? 35 : 0)
+  );
+
+  securityIndicatorBarEl.style.width = `${percent}%`;
+
+  if (percent > 69) {
+    securityIndicatorBarEl.classList.remove("critical");
+    securityIndicatorBarEl.classList.remove("warning");
+    securityIndicatorBarEl.classList.add("safe");
+  } else if (percent > 50) {
+    securityIndicatorBarEl.classList.remove("critical");
+    securityIndicatorBarEl.classList.add("warning");
+    securityIndicatorBarEl.classList.remove("safe");
+  } else {
+    securityIndicatorBarEl.classList.add("critical");
+    securityIndicatorBarEl.classList.remove("warning");
+    securityIndicatorBarEl.classList.remove("safe");
+  }
+
+  if (percent >= 100) {
+    securityIndicatorBarEl.classList.add("completed");
+  } else {
+    securityIndicatorBarEl.classList.add("remove");
+  }
+}
+
+function calculateFontSize() {
+  if (passwordLength > 45) {
+    inputEl.classList.remove("font-sm");
+    inputEl.classList.remove("font-xs");
+    inputEl.classList.add("font-xxs");
+  } else if (passwordLength > 32) {
+    inputEl.classList.remove("font-sm");
+    inputEl.classList.add("font-xs");
+    inputEl.classList.remove("font-xxs");
+  } else if (passwordLength > 22) {
+    inputEl.classList.add("font-sm");
+    inputEl.classList.remove("font-xs");
+    inputEl.classList.remove("font-xxs");
+  } else {
+    inputEl.classList.remove("font-sm");
+    inputEl.classList.remove("font-xs");
+    inputEl.classList.remove("font-xxs");
+  }
 }
 
 function copy() {
@@ -22,13 +97,20 @@ function copy() {
 const passwordLengthEl = document.querySelector("#password-length");
 passwordLengthEl.addEventListener("input", function () {
   passwordLength = passwordLengthEl.value;
+  document.querySelector("#password-length-text").innerText = passwordLength;
   generatePassword();
 });
+
+upperCaseCheckEl.addEventListener("click", generatePassword);
+numberCheckEl.addEventListener("click", generatePassword);
+symbolCheckEl.addEventListener("click", generatePassword);
 
 const copyButtonEl1 = document.querySelector("#copy-1");
 copyButtonEl1.addEventListener("click", copy);
 
 const copyButtonEl2 = document.querySelector("#copy-2");
 copyButtonEl2.addEventListener("click", copy);
+
+document.querySelector("#renew").addEventListener("click", generatePassword);
 
 generatePassword();
